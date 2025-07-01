66c862952b3148d2248fc9058f8ff35a49ec243b
LRYP <2213469156@qq.com>
2025-07-01 23:34:47 +0800

    自动提交：更新 UserLogoutMessagePlanner (APIPlan) 的代码

--- src/main/scala/Impl/UserLogoutMessagePlanner.scala
+++ src/main/scala/Impl/UserLogoutMessagePlanner.scala
@@ -1,22 +1,26 @@
 package Impl
 
-/**
- * UserLogoutMessagePlanner: 用户登出操作的实现
- * 输入参数: userToken : String
- * 输出参数: result : String
- */
 
-import Common.API.{PlanContext, Planner}
-import Common.DBAPI._
-import Common.Object.SqlParameter
-import Common.ServiceUtils.schemaName
+/**
+ * Planner for UserLogoutMessage: 用户传入token完成登出操作。
+ */
 import Utils.TokenProcess.invalidateToken
+import Common.API.{PlanContext, Planner}
+import Common.Object.SqlParameter
+import Common.DBAPI._
+import Common.ServiceUtils.schemaName
 import cats.effect.IO
 import org.slf4j.LoggerFactory
 import io.circe._
 import io.circe.syntax._
 import io.circe.generic.auto._
 import org.joda.time.DateTime
+import cats.implicits._
+import Common.Serialize.CustomColumnTypes.{decodeDateTime, encodeDateTime}
+import io.circe._
+import io.circe.syntax._
+import io.circe.generic.auto._
+import org.joda.time.DateTime
 import cats.implicits.*
 import Common.DBAPI._
 import Common.API.{PlanContext, Planner}
@@ -25,49 +29,30 @@ import Common.Object.SqlParameter
 import Common.Serialize.CustomColumnTypes.{decodeDateTime,encodeDateTime}
 import Common.ServiceUtils.schemaName
 import Utils.TokenProcess.invalidateToken
-import io.circe._
-import io.circe.syntax._
-import io.circe.generic.auto._
-import org.joda.time.DateTime
 import cats.implicits.*
 import Common.Serialize.CustomColumnTypes.{decodeDateTime,encodeDateTime}
 
-case class UserLogoutMessagePlanner(
-                                      userToken: String,
-                                      override val planContext: PlanContext
-                                    ) extends Planner[String] {
-  private val logger = LoggerFactory.getLogger(this.getClass.getSimpleName + "_" + planContext.traceID.id)
+case class UserLogoutMessagePlanner(userToken: String)(override val planContext: PlanContext) extends Planner[String] {
+  val logger = LoggerFactory.getLogger(this.getClass.getSimpleName + "_" + planContext.traceID.id)
 
-  override def plan(using PlanContext): IO[String] = {
+  override def plan(using planContext: PlanContext): IO[String] = {
+    logger.info(s"开始处理用户登出操作，传入的token为：${userToken}")
+
     for {
-      // Step 1: Log token invalidation progress
-      _ <- IO(logger.info(s"[Step 1] 开始注销用户的Token: ${userToken}"))
-      
-      // Step 2: Perform token invalidation
-      isInvalidated <- performTokenInvalidation(userToken)
-      _ <- IO(logger.info(s"[Step 2] Token [${userToken}] 是否成功无效化: ${isInvalidated}"))
-      
-      // Step 3: Return the result based on the invalidation outcome
+      // Step 1: 使用户Token无效化
+      _ <- IO(logger.info(s"[Step 1] 调用invalidateToken方法，尝试使Token无效"))
+      isInvalidated <- invalidateToken(userToken)
+
+      // 根据无效化结果生成登出消息
       resultMessage <- if (isInvalidated) {
-        IO(logger.info(s"[Step 3] Token [${userToken}] 已成功登出")) >>
-        IO("登出成功！")
+        IO(logger.info(s"[Step 1] Token [${userToken}] 无效化操作成功")) >>
+          IO("登出成功！")
       } else {
-        IO(logger.warn(s"[Step 3] Token [${userToken}] 登出失败")) >>
-        IO("登出失败！")
+        IO(logger.info(s"[Step 1] Token [${userToken}] 无效化操作失败")) >>
+          IO("登出失败，请检查Token是否有效！")
       }
-    } yield resultMessage
-  }
 
-  /**
-   * Helper function to perform token invalidation using the library method.
-   * @param token The user token to invalidate.
-   * @return IO[Boolean] indicating whether the invalidation was successful.
-   */
-  private def performTokenInvalidation(token: String)(using PlanContext): IO[Boolean] = {
-    for {
-      _ <- IO(logger.info(s"[Helper] 即将调用invalidateToken方法使token无效化: ${token}"))
-      isInvalidated <- invalidateToken(token)
-      _ <- IO(logger.info(s"[Helper] 调用invalidateToken完成，结果: ${isInvalidated}"))
-    } yield isInvalidated
+      _ <- IO(logger.info(s"[结束] 用户Token [${userToken}] 登出结果信息: ${resultMessage}"))
+    } yield resultMessage
   }
 }
