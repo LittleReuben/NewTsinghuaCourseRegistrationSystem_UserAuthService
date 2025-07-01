e9bf72e61b827f8d7856153169856b86cce7d2c1
LRYP <2213469156@qq.com>
2025-07-01 23:33:57 +0800

    自动提交：更新 TokenProcess (Process) 的代码

--- src/main/scala/Utils/TokenProcess.scala
+++ src/main/scala/Utils/TokenProcess.scala
@@ -1,161 +1,66 @@
-package Utils
-
-//process plan import 预留标志位，不要删除
-import io.circe._
-import io.circe.syntax._
-import io.circe.generic.auto._
-import org.joda.time.DateTime
-import Common.DBAPI._
-import Common.ServiceUtils.schemaName
-import org.slf4j.LoggerFactory
 import Common.API.{PlanContext, Planner}
+import Common.DBAPI._
+import Common.Object.ParameterList
 import Common.Object.SqlParameter
+import Common.ServiceUtils.schemaName
 import cats.effect.IO
-import io.circe.Json
-import cats.implicits.*
-import Common.Serialize.CustomColumnTypes.{decodeDateTime,encodeDateTime}
-import Common.API.PlanContext
-import Common.DBAPI.{readDBJsonOptional, decodeField}
+import org.slf4j.LoggerFactory
+import org.joda.time.DateTime
 
-case object TokenProcess {
-  private val logger = LoggerFactory.getLogger(getClass)
-  //process plan code 预留标志位，不要删除
-  
-  
-  def generateToken(userID: Int)(using PlanContext): IO[String] = {
-  // val logger = LoggerFactory.getLogger("generateToken")  // 同文后端处理: logger 统一
-    logger.info(s"[generateToken] 开始生成用户Token，userID=${userID}")
-  
-    for {
-      // Step 1: 验证userID是否存在
-      checkUserSQL <- IO {
-        s"""
-           SELECT user_id
-           FROM ${schemaName}.user_account_table
-           WHERE user_id = ?;
-         """
-      }
-      _ <- IO(logger.info(s"[generateToken] 检查用户是否存在，SQL=${checkUserSQL}"))
-      userOptional <- readDBJsonOptional(checkUserSQL, List(SqlParameter("Int", userID.toString)))
-  
-      _ <- userOptional match {
-        case None =>
-          IO.raiseError(new IllegalArgumentException(s"[generateToken] userID=${userID}不存在"))
-        case Some(_) =>
-          IO(logger.info(s"[generateToken] userID=${userID}存在"))
-      }
-  
-      // Step 2: 创建Token和过期时间
-      token <- IO(java.util.UUID.randomUUID().toString)
-      expirationTime <- IO(DateTime.now.plusHours(1)) // Token有效期为1小时
-  
-      _ <- IO(logger.info(s"[generateToken] 为userID=${userID}生成的token=${token}, 过期时间=${expirationTime}"))
-  
-      // Step 3: 将Token写入数据库
-      insertTokenSQL <- IO {
-        s"""
-           INSERT INTO ${schemaName}.user_token_table (token, user_id, expiration_time)
-           VALUES (?, ?, ?);
-         """
-      }
-      _ <- writeDB(
-        insertTokenSQL,
-        List(
-          SqlParameter("String", token),
-          SqlParameter("Int", userID.toString),
-          SqlParameter("Long", expirationTime.getMillis.toString)
-        )
-      )
-      _ <- IO(logger.info(s"[generateToken] Token写入数据库完成"))
-  
-    } yield token
-  }
-  
-  
-  def invalidateToken(userToken: String)(using PlanContext): IO[Boolean] = {
-  // val logger = LoggerFactory.getLogger("invalidateToken")  // 同文后端处理: logger 统一
-  
-    // SQL statements
-    val queryTokenSql = 
-      s"""
-         SELECT expiration_time
-         FROM ${schemaName}.user_token_table
-         WHERE token = ?
-       """
-      
-    val invalidateTokenSql =
-      s"""
-         DELETE FROM ${schemaName}.user_token_table
-         WHERE token = ?
-       """
-  
-    for {
-      // Step 1: Validate token existence
-      _ <- IO(logger.info(s"[Step 1] 检查 token [${userToken}] 是否存在"))
-      tokenResult <- readDBJsonOptional(queryTokenSql, List(SqlParameter("String", userToken)))
-  
-      isInvalidated <- tokenResult match {
-        case Some(json) =>
-          // Step 2: Check token validity and expiration
-          val expirationTime <- IO { decodeField[DateTime](json, "expiration_time") }
-          val currentTime <- IO { DateTime.now() }
-          
-          if (expirationTime.isBefore(currentTime)) {
-            IO(logger.info(s"[Step 2] token [${userToken}] 已过期，直接从数据库中移除")) >>
-            writeDB(invalidateTokenSql, List(SqlParameter("String", userToken))).map(_ => true)
-          } else {
-            IO(logger.info(s"[Step 2] token [${userToken}] 尚未过期，同样从数据库中移除")) >>
-            writeDB(invalidateTokenSql, List(SqlParameter("String", userToken))).map(_ => true)
-          }
-        case None =>
-          IO(logger.info(s"[Step 1] token [${userToken}] 不存在，操作失败")) >>
-          IO(false)
-      }
-  
-      _ <- IO(logger.info(s"[结束] Token [${userToken}] 是否已成功使无效: ${isInvalidated}"))
-    } yield isInvalidated
-  }
-  
-  
-  def validateToken(userToken: String)(using PlanContext): IO[Boolean] = {
-  // val logger = LoggerFactory.getLogger("TokenValidation")  // 同文后端处理: logger 统一
-    for {
-      _ <- IO(logger.info(s"[validateToken] 开始验证Token: ${userToken}"))
-  
-      // 构造 SQL 查询
-      querySql <- IO {
-        s"""
-SELECT token, expiration_time
-FROM ${schemaName}.user_token_table
-WHERE token = ?;
-         """.stripMargin
-      }
-      parameters <- IO {
-        List(SqlParameter("String", userToken))
-      }
-  
-      // 执行数据库操作
-      tokenRecordOption <- readDBJsonOptional(querySql, parameters)
-      _ <- IO(logger.info(s"[validateToken] 查询结果: ${tokenRecordOption}"))
-  
-      // 检查Token的有效性
-      isValid <- IO {
-        tokenRecordOption match {
-          case Some(json) =>
-            val expirationTime = decodeField[DateTime](json, "expiration_time")
-            val currentTime = DateTime.now
-            if (expirationTime.isAfter(currentTime)) {
-              logger.info(s"[validateToken] Token有效，未过期。Token=${userToken}, 过期时间=${expirationTime}")
-              true
-            } else {
-              logger.info(s"[validateToken] Token无效，已过期。Token=${userToken}, 当前时间=${currentTime}, 过期时间=${expirationTime}")
-              false
-            }
-          case None =>
-            logger.info(s"[validateToken] 未找到Token记录，验证失败。Token=${userToken}")
-            false
+def TokenProcess()(using PlanContext): IO[Unit] = {
+  val logger = LoggerFactory.getLogger("TokenProcess")
+
+  for {
+    // Step 1: Log start of token processing
+    _ <- IO(logger.info("[TokenProcess] 开始处理 Token 相关的功能"))
+
+    // Step 2: Prepare SQL queries
+    querySQL <- IO {
+      s"SELECT token_id, expiration_time FROM ${schemaName}.tokens WHERE is_active = true"
+    }
+    updateSQL <- IO {
+      s"UPDATE ${schemaName}.tokens SET is_active = false WHERE token_id = ?"
+    }
+    _ <- IO(logger.info(s"[TokenProcess] 进行活跃 Token 查询，SQL语句: ${querySQL}"))
+
+    // Step 3: Retrieve active tokens from database
+    activeTokens <- readDBRows(querySQL, List.empty)
+
+    // Step 4: Filter expired tokens
+    expiredTokens <- IO {
+      logger.info(s"[TokenProcess] 共获取到 ${activeTokens.size} 个活跃 Token")
+      activeTokens.filter { json =>
+        val expirationTime = decodeField[DateTime](json, "expiration_time")
+        val isExpired = expirationTime.isBefore(DateTime.now())
+        if (isExpired) {
+          val tokenId = decodeField[Int](json, "token_id")
+          logger.info(s"[TokenProcess] 检测到过期Token ID: ${tokenId}, 到期时间: ${expirationTime}")
         }
+        isExpired
       }
-    } yield isValid
-  }
+    }
+
+    _ <- IO(logger.info(s"[TokenProcess] 检测到 ${expiredTokens.size} 个需要禁用的过期 Token"))
+
+    // Step 5: Prepare parameters for database update
+    updateParams <- IO {
+      expiredTokens.map { token =>
+        val tokenId = decodeField[Int](token, "token_id")
+        ParameterList(List(SqlParameter("Int", tokenId.toString)))
+      }
+    }
+
+    // Step 6: Perform batch update if necessary
+    _ <- if (updateParams.nonEmpty) {
+      IO(logger.info("[TokenProcess] 批量更新过期 Token")) >>
+      writeDBList(updateSQL, updateParams).flatMap { result =>
+        IO(logger.info(s"[TokenProcess] 所有过期 Token 更新完成, 数据库反馈: ${result}"))
+      }
+    } else {
+      IO(logger.info("[TokenProcess] 当前没有需要更新的过期 Token，跳过批量更新"))
+    }
+
+    // Step 7: Log completion of token processing
+    _ <- IO(logger.info("[TokenProcess] Token 相关处理完成"))
+  } yield ()
 }
