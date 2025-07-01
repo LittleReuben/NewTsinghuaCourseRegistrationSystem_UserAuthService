83b3c8f24504e5c2ea709d257d5ddd9ab6d7f71b
LRYP <2213469156@qq.com>
2025-07-01 23:34:27 +0800

    自动提交：更新 TokenProcess (Process) 的代码

--- src/main/scala/Utils/TokenProcess.scala
+++ src/main/scala/Utils/TokenProcess.scala
@@ -1,7 +1,6 @@
 import Common.API.{PlanContext, Planner}
-import Common.DBAPI._
-import Common.Object.ParameterList
-import Common.Object.SqlParameter
+import Common.DBAPI.{decodeField, readDBRows, writeDBList}
+import Common.Object.{SqlParameter, ParameterList}
 import Common.ServiceUtils.schemaName
 import cats.effect.IO
 import org.slf4j.LoggerFactory
@@ -11,56 +10,51 @@ def TokenProcess()(using PlanContext): IO[Unit] = {
   val logger = LoggerFactory.getLogger("TokenProcess")
 
   for {
-    // Step 1: Log start of token processing
-    _ <- IO(logger.info("[TokenProcess] 开始处理 Token 相关的功能"))
+    _ <- IO(logger.info("[TokenProcess] 开始处理与Token相关的功能"))
 
-    // Step 2: Prepare SQL queries
-    querySQL <- IO {
-      s"SELECT token_id, expiration_time FROM ${schemaName}.tokens WHERE is_active = true"
+    querySql <- IO {
+      s"""
+      SELECT token_id, last_used
+      FROM ${schemaName}.token
+      WHERE status = ? AND expires_at > ?;
+      """
     }
-    updateSQL <- IO {
-      s"UPDATE ${schemaName}.tokens SET is_active = false WHERE token_id = ?"
-    }
-    _ <- IO(logger.info(s"[TokenProcess] 进行活跃 Token 查询，SQL语句: ${querySQL}"))
+    statusParam <- IO(SqlParameter("String", "active"))
+    expiresAtParam <- IO(SqlParameter("DateTime", DateTime.now().getMillis.toString))
+    
+    _ <- IO(logger.info(s"[TokenProcess] 执行查询有效Token的SQL: ${querySql}"))
 
-    // Step 3: Retrieve active tokens from database
-    activeTokens <- readDBRows(querySQL, List.empty)
+    tokens <- readDBRows(querySql, List(statusParam, expiresAtParam))
+    
+    tokenUpdates <- IO {
+      tokens.map { json =>
+        val tokenId = decodeField[Int](json, "token_id")
+        val lastUsed = decodeField[DateTime](json, "last_used")
+        logger.info(s"[TokenProcess] Token ID: ${tokenId} 上次使用时间: ${lastUsed}")
 
-    // Step 4: Filter expired tokens
-    expiredTokens <- IO {
-      logger.info(s"[TokenProcess] 共获取到 ${activeTokens.size} 个活跃 Token")
-      activeTokens.filter { json =>
-        val expirationTime = decodeField[DateTime](json, "expiration_time")
-        val isExpired = expirationTime.isBefore(DateTime.now())
-        if (isExpired) {
-          val tokenId = decodeField[Int](json, "token_id")
-          logger.info(s"[TokenProcess] 检测到过期Token ID: ${tokenId}, 到期时间: ${expirationTime}")
-        }
-        isExpired
+        val newLastUsed = DateTime.now()
+        ParameterList(List(
+          SqlParameter("Int", tokenId.toString),
+          SqlParameter("DateTime", newLastUsed.getMillis.toString)
+        ))
       }
     }
-
-    _ <- IO(logger.info(s"[TokenProcess] 检测到 ${expiredTokens.size} 个需要禁用的过期 Token"))
-
-    // Step 5: Prepare parameters for database update
-    updateParams <- IO {
-      expiredTokens.map { token =>
-        val tokenId = decodeField[Int](token, "token_id")
-        ParameterList(List(SqlParameter("Int", tokenId.toString)))
-      }
+    
+    updateSql <- IO {
+      s"""
+      UPDATE ${schemaName}.token
+      SET last_used = ?
+      WHERE token_id = ?;
+      """
     }
-
-    // Step 6: Perform batch update if necessary
-    _ <- if (updateParams.nonEmpty) {
-      IO(logger.info("[TokenProcess] 批量更新过期 Token")) >>
-      writeDBList(updateSQL, updateParams).flatMap { result =>
-        IO(logger.info(s"[TokenProcess] 所有过期 Token 更新完成, 数据库反馈: ${result}"))
-      }
-    } else {
-      IO(logger.info("[TokenProcess] 当前没有需要更新的过期 Token，跳过批量更新"))
+    
+    _ <- tokenUpdates match {
+      case Nil => IO(logger.info("[TokenProcess] 无有效Token，无需更新"))
+      case updates =>
+        IO(logger.info(s"[TokenProcess] 准备更新Token上次使用时间，共更新 ${updates.size} 条记录")) >>
+        writeDBList(updateSql, updates).void
     }
-
-    // Step 7: Log completion of token processing
-    _ <- IO(logger.info("[TokenProcess] Token 相关处理完成"))
+    
+    _ <- IO(logger.info("[TokenProcess] Token处理完成"))
   } yield ()
 }
